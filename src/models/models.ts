90adc64ae9ee322f1de2e0e360d356ed7ff13e65
MTanrivermis <tanrivermis.mehmet@gmail.com>
2024-08-22 14:35:27 +0300

    up

---
+++ src/models/models.ts
@@ -0,0 +1,22 @@
+export type EventFunc= (e:React.ChangeEvent<HTMLInputElement>) => void;
+
+export interface Products {
+    products: Product[];
+    total: number;
+    skip: number; 
+    limit: number;
+}
+
+export interface Product {
+    id:                 number;
+    title:              string;
+    category:           string;
+    description:        string;
+    price:              number;
+    discountPercentage: number;
+    rating:             number;
+    stock:              number;
+    brand:              string;
+    thumbnail:          string;
+    images:             string[];
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,68 @@
-
+import axios from "axios";
+import SearchComp from "../components/SearchComp";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import {
+  fetchFail,
+  fetchStart,
+  getSuccessProduct,
+} from "../features/productSlice";
+import { EventFunc, Products } from "../models/models";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const dispatch = useAppDispatch();
+  const { loading, error, productsList } = useAppSelector(
+    (state) => state.products
+  );
 
-export default Home
+  const getData = async () => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.get<Products>(
+        `https://dummyjson.com/products/search?q=${search}`
+      );
+      console.log(data);
+      dispatch(getSuccessProduct(data.products));
+    } catch (error) {
+      dispatch(fetchFail());
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [search]);
+
+  // const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  //   setSearch(e.target.value)
+  // }
+
+  const handleChange: EventFunc = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div>
+      <SearchComp handleChange={handleChange} />
+      {loading ? (
+        error ? (
+          <div>
+            <p className="text-center text-red-600"> Something went wrong...</p>
+          </div>
+        ) : (
+          <div>
+            <p className="text-center text-red-600"> Products Loading...</p>
+          </div>
+        )
+      ) : (
+        <div>
+          {productsList.map((item) => (
+            <p>{item.title}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
